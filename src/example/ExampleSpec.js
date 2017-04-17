5fbb2838ea809d2938c2519747e74bd31e13c2a2
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-04-17 19:51:08 +0300

    Success build

---
+++ src/example/ExampleSpec.js
@@ -0,0 +1,32 @@
+// var React = require('react');
+// var TestUtils = require('react-dom/test-utils'); //I like using the Test Utils, but you can just use the DOM API instead.
+// var expect = require('expect');
+// var Example = require('./Example'); //my root-test lives in components/__tests__/, so this is how I require in my components.
+
+import React from 'react';
+import expect from 'expect';
+import Example from './Example';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+
+window.rctu = ReactTestUtils;
+
+describe('Example', () => {
+  let component;
+
+  beforeEach(() => {
+    component = ReactTestUtils.renderIntoDocument(
+      <Example option="Component example option" />
+    );
+  });
+  
+  it('should display the correct example option', () => {
+    expect(ReactDOM.findDOMNode(component).textContent)
+      .toMatch(/Hello, it's Example/);
+  });
+
+  it('should have "option" property', () => {
+    expect(component).toNotEqual(undefined);
+  });
+    
+});
